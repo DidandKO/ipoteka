7fe77a0573d760aa821be56a66a24d9e8af5bd83
Nazar <didenko.nazar@list.ru>
2022-09-14 13:46:48 +0700

    UnitTest

--- backend/MorOC/tests.py
+++ backend/MorOC/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class BankOffersApiTestCase(APITestCase):
+    def test_get(self):
+        url = 'http://127.0.0.1:8000/api/bank_offers/'
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": password,
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "localhost",
         "PORT": 5432,
         "CONN_MAX_AGE": 600,
